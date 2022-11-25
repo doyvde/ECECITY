5b69ae8cd07890f1e455dac7c7e2af463ca2a218
vinciguyonneau <95126653+vinciguyonneau@users.noreply.github.com>
2022-11-25 19:06:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,25 +1,19 @@
 #include "mabiblio.h"
 
-///Source Majeure: https://github.com/iNaDeX/ECE-City   2018
-///Source: https://github.com/MaximeJUMELINE/ECE_CITY_2O12   2012
-
 t_graphismes* graphs;
 
 int main()
 {
     srand(time(NULL));
-    initialiser_allegro(1024, 768);
-
-    graphs = graphismes_charger();
-
-    t_graphMenu graphs_menu;
-    menu_charger(&graphs_menu);
-
-    menu_afficher(graphs_menu);
-
-    menu_liberer(&graphs_menu);
-    graphismes_liberer(graphs);
+    initialiser_allegro(1024, 768);//initialisation d'Allegro
+    BITMAP* menu1=chargerImage("fichiers/images/menu/menu1/menuChoixLangues.bmp");//chargement du menu de choix de la langue
+    graphs = graphismes_charger();//chargement des graphismes
+    t_graphMenu graphs_menu;//structure menu pour le son
+    menu(menu1, &graphs_menu);//menu et jeu
+    graphismes_liberer(graphs);//libération des graphismes
     fermer_allegro();
-
     return 0;
 }END_OF_MAIN();
+
+///Source Majeure: https://github.com/iNaDeX/ECE-City   2018
+///Source: https://github.com/MaximeJUMELINE/ECE_CITY_2O12   2012
